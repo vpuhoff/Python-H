8e5d7d1baf2bad9e66f11fab9c03e8454d9d1001
vpuhoff <vpuhoff@live.ru>
2018-07-07 12:13:59 +0300

    Исправления

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ import logging
 import os
 import hvac
 
-client = hvac.Client(url='http://80.211.91.158:8200', token=os.environ['VAULT_TOKEN'])
+vault = hvac.Client(url='http://80.211.91.158:8200', token=os.environ['VAULT_TOKEN'])
 
-#smtp = client.read('secret/ekaterina-gadanie.com')['data']
 
 from flask import Flask, render_template, request
+import raven
 
+sentry_data = vault.read('secret/sentry')['data']
+gate = vault.read('secret/automate-cloud')['data']
 
-# [START config]
-#SENDGRID_API_KEY = os.environ['SENDGRID_API_KEY']
-#SENDGRID_SENDER = os.environ['SENDGRID_SENDER']
-# [END config]
+sentry = raven.Client(sentry_data['url'])
+sentry.captureMessage('Restart application!',level='info')
 
 app = Flask(__name__)
 import string
@@ -30,19 +30,26 @@ class Del:
 DD = Del()
 
 def SendSMS(phone_raw, message):
-    phone = '+'+phone_raw.translate(DD)
-    url = 'https://llamalab.com/automate/cloud/message'
+    try:
+        phone = '+'+phone_raw.translate(DD)
+        url = gate['url']
 
-    headers = {'Content-type': 'application/json'}
-    payload =json.dumps({
-        "secret": "1.IsCmZnfHe-m-gF2DB6lsSvtkaM6R0uNsLyVLs1RSvWA=",
-        "to": "vpuhoff92@gmail.com",
-        "device":None,
-        "payload": phone+";"+message.replace(';',':')
-    })
-    r = requests.post(url, data=payload, headers=headers)
-    return r.reason
-
+        headers = {'Content-type': 'application/json'}
+        payload =json.dumps({
+            "secret": gate['secret'],
+            "to": gate['worker'],
+            "device":None,
+            "payload": phone+";"+message.replace(';',':')
+        })
+        r = requests.post(url, data=payload, headers=headers)
+        sentry.captureMessage('SMS Sended!',extra=
+        {'phone':phone,
+        'raw_phone':phone_raw,
+        'message':message} )
+        return r.reason
+    except Exception:
+        sentry.captureException()
+    
 @app.route('/gate')
 def gate():
     return render_template('index.html')
@@ -53,20 +60,21 @@ def index():
 
 @app.route('/gate/callback_form')
 def callback_form():
+    sentry.captureMessage('Render Callback form!',level='info')
     return render_template('callback_form.html')    
 
 @app.route('/gate/callback', methods=['POST'])
 def callback():
-    to = '+79243132456'
-    message = request.form.get('name')+': '+request.form.get('phone')
-    if not to:
-        return ('Please enter phone number '), 400
     try:
+        to = '+79243132456'
+        message = request.form.get('name')+': '+request.form.get('phone')
+        if not to:
+            return ('Please enter phone number '), 400
         SendSMS(to,'Заказ звонка: '+message)
     except Exception as e:
-        SendSMS(to,'Error: {}'.format(e))
+        sentry.captureException(
+            extra={'Form data':request.form})
         return 'An error occurred: {}'.format(e), 500
-
     return render_template('callback_ok.html')
 
 
@@ -80,6 +88,7 @@ def send_sms():
     if not message:
         return ('Please enter message'), 400
     try:
+        sentry.captureMessage('Send SMS via API',level='info', extra={'to':to,'message':message})
         SendSMS(to,message)
     except Exception as e:
         return 'An error occurred: {}'.format(e), 500
